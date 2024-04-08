4d9b0e5b6b5b68eb13af8f865cc6d388a5d1902c
Idris Raja <idris.raja@gmail.com>
2024-04-08 10:06:13 -0500

    add haskell template

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
         path = ./templates/latex;
         description = "A flake you can use to kickstart a latex project";
       };
+      haskell = {
+        path = ./templates/haskell;
+        description = "A flake you can use to kickstart a haskell project";
+      };
     };
   };
 }
