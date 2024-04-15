1c6323ba30ed1f7b1b306e5261a0fc867423ee04
Idris Raja <idris.raja@gmail.com>
2024-04-15 08:03:15 -0500

    fix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
         path = ./templates/haskell;
         description = "A flake you can use to kickstart a haskell project";
       };
+      basic = {
+        path = ./templates/basic;
+        description = "A flake you can use to kickstart a bare-bones project";
+      };
     };
   };
 }
