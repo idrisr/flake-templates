e7a555813b9e91b1d1fbf7266492f7e5da34f5cd
Idris Raja <idris.raja@gmail.com>
2025-07-29 21:44:04 -0500

    remove devenv.  update ghc

--- templates/haskell/flake.nix
+++ templates/haskell/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -8,8 +8,24 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       system = flake-utils.lib.system.x86_64-linux;
-      compiler = "ghc96";
+      compiler = "ghc984";
       renameme =
         pkgs.haskell.packages.${compiler}.callCabal2nix "" ./renameme { };
-    in { packages.${system}.default = renameme; };
+    in
+    {
+      packages.${system}.default = renameme;
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs =
+          with pkgs.haskell.packages."${compiler}"; [
+            fourmolu
+            cabal-fmt
+            implicit-hie
+            ghcid
+            cabal2nix
+            ghc
+            pkgs.ghciwatch
+            cabal-install
+          ];
+      };
+    };
 }
