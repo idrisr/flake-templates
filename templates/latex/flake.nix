3ac4a803ccefa8538e45d03a74c3b37f75c89333
Idris Raja <idris.raja@gmail.com>
2025-08-16 13:51:18 -0500

    bump nixpkgs. use flake utils eachDefaultSystem

--- templates/latex/flake.nix
+++ templates/latex/flake.nix
@@ -1,19 +1,23 @@
 {
   description = "DevShell for LaTeX + Pygments";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "latex-shell";
-        packages = [ pkgs.texliveFull pkgs.python312Packages.pygments ];
-        shellHook = ''
-          export LATEXINDENT_CONFIG=indentconfig.yaml
-        '';
-      };
-    };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "latex-shell";
+          packages = with pkgs; [
+            texliveFull
+            python312Packages.pygments
+          ];
+          shellHook = ''
+            export LATEXINDENT_CONFIG=indentconfig.yaml
+          '';
+        };
+      });
 }
