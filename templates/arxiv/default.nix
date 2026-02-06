ca1fae63202cfceb0cd72ab4d1ba069badb42d4c
Idris Raja <idris.raja@gmail.com>
2026-02-06 10:54:51 -0600

    add arxiv

---
+++ templates/arxiv/default.nix
@@ -0,0 +1,40 @@
+{ pkgs, stdenvNoCC, python312Packages }:
+let
+  mytex = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      biblatex
+      cm-super
+      enumitem
+      glossaries
+      latex-bin
+      latexmk
+      minted
+      pdfcol
+      rsfs
+      scheme-small
+      tcolorbox
+      tikzfill
+      titlesec
+      upquote
+      xcolor
+      ;
+  };
+in
+
+stdenvNoCC.mkDerivation {
+  name = throw "change name";
+  pname = throw "change name";
+  src = ./src;
+  nativeBuildInputs = [
+    mytex
+    python312Packages.pygments
+    pkgs.biber
+  ];
+  buildPhase = ''
+    latexmk 00-main.tex
+  '';
+  installPhase = ''
+    mkdir -p $out
+    mv build/00-main.pdf $out/
+  '';
+}
