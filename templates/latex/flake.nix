c23a3dc1e9a4e5221ce37a6b1349d347db521296
Idris Raja <idris.raja@gmail.com>
2026-02-19 08:42:03 -0600

    improve flake

--- templates/latex/flake.nix
+++ templates/latex/flake.nix
@@ -3,21 +3,33 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "latex-shell";
-          packages = with pkgs; [
-            texliveFull
-            python312Packages.pygments
-          ];
-          shellHook = ''
-            export LATEXINDENT_CONFIG=indentconfig.yaml
-          '';
-        };
-      });
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem
+      [ "aarch64-darwin" "x86_64-linux" ]
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          packages = {
+            default = pkgs.callPackage ./. { };
+          };
+
+          checks = {
+            default = self.packages.${system}.default;
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              name = "latex-shell";
+              packages = with pkgs; [
+                texliveFull
+                python312Packages.pygments
+              ];
+              shellHook = ''
+                export LATEXINDENT_CONFIG=indentconfig.yaml
+              '';
+            };
+          };
+        });
 }
